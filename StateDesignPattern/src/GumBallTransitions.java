cb0b895655ad333956fa99abc377e7e7f7947c45
Snichat97 <34672064+Snichat97@users.noreply.github.com>
2021-09-24 12:42:14 -0700

    Add files via upload

---
+++ StateDesignPattern/src/GumBallTransitions.java
@@ -0,0 +1,7 @@
+public interface GumBallTransitions {
+    public void insertsQuarter();
+    public void ejectsQuarter();
+    public void turnsCrank();
+    public void dispenseGumball();
+    public void indicator();
+}
---
+++ StateDesignPattern/src/Start.java
@@ -0,0 +1,12 @@
+public class Start {
+    public static void main(String[] args) {
+        GumBallManager GumBallManager = new GumBallManager();
+        GumBallManager.doTurnsCrank();
+        GumBallManager.doInsertsQuarter();
+        GumBallManager.doInsertsQuarter();
+        GumBallManager.doInsertsQuarter();
+        GumBallManager.doInsertsQuarter();
+        GumBallManager.doTurnsCrank();
+        System.out.println("Hello, World!");
+    }
+}
